6d4b013eb48e9361019bf97a6f3e79f6c3fef73b
JasonJason1 <126553348+JasonJason1@users.noreply.github.com>
2023-06-08 10:47:09 +0700

    Add files via upload

---
+++ camtofirebasetest/src/secrets.h
@@ -0,0 +1,11 @@
+#define ssid "Ye"
+#define password "1234asdf"
+#define FIREBASE_HOST "https://triot-d7e6e-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "p6MtqXkstPUvFZLr61JAjAD4DAMUD97Spp4kE3PC"
+
+// // Insert Authorized Email and Corresponding Password
+// #define USER_EMAIL "REPLACE_WITH_THE_AUTHORIZED_USER_EMAIL"
+// #define USER_PASSWORD "REPLACE_WITH_THE_AUTHORIZED_USER_PASSWORD"
+
+// Insert Firebase storage bucket ID e.g bucket-name.appspot.com
+#define STORAGE_BUCKET_ID "triot-d7e6e.appspot.com"
