6144115ec7c1f377b8020d48758165033642bc94
JasonJason1 <126553348+JasonJason1@users.noreply.github.com>
2023-06-27 19:12:46 +0700

    Update secrets.h

--- camtofirebasetest/src/secrets.h
+++ camtofirebasetest/src/secrets.h
@@ -1,11 +1,7 @@
-#define ssid "Ye"
-#define password "1234asdf"
-#define FIREBASE_HOST "https://triot-d7e6e-default-rtdb.firebaseio.com/"
-#define FIREBASE_AUTH "p6MtqXkstPUvFZLr61JAjAD4DAMUD97Spp4kE3PC"
-
-// // Insert Authorized Email and Corresponding Password
-// #define USER_EMAIL "REPLACE_WITH_THE_AUTHORIZED_USER_EMAIL"
-// #define USER_PASSWORD "REPLACE_WITH_THE_AUTHORIZED_USER_PASSWORD"
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+#define FIREBASE_HOST "FIREBASE_HOST"
+#define FIREBASE_AUTH "FIREBASE_AUTH"
 
 // Insert Firebase storage bucket ID e.g bucket-name.appspot.com
-#define STORAGE_BUCKET_ID "triot-d7e6e.appspot.com"
+#define STORAGE_BUCKET_ID "STORAGE_BUCKET"
